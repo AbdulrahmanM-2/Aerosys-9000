146541fb7e4ac30f8c2e2fa4bd83fd60884bef38
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-28 23:07:51 +0300

    Add files via upload

--- aerosys-v4/telemetry-gateway/server.js
+++ aerosys-v4/telemetry-gateway/server.js
@@ -1,1 +1,18 @@
 
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({port:4000});
+
+setInterval(()=>{
+
+ const data = {
+  altitude:10000+Math.random()*100,
+  speed:250+Math.random()*5,
+  pitch:Math.random()*4
+ }
+
+ wss.clients.forEach(client=>{
+  client.send(JSON.stringify(data))
+ })
+
+},200);
