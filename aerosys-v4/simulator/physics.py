b6e16fefa9a722499c18db9a5080162abdcdb35a
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-28 23:09:08 +0300

    Add files via upload

--- aerosys-v4/simulator/physics.py
+++ aerosys-v4/simulator/physics.py
@@ -1,1 +1,12 @@
 
+import random
+
+def update(state, control):
+    state["pitch"] += control.get("elevator",0) * 0.02
+    state["roll"] += control.get("aileron",0) * 0.02
+    return state
+
+if __name__ == "__main__":
+    state = {"pitch":0,"roll":0}
+    control = {"elevator":0.3,"aileron":0.1}
+    print(update(state,control))
